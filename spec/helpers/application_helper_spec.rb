9b99818bda3097eaf5d66c7f7e9e17f614e30c34
rezerbit <rezerbit@gmail.com>
2018-12-13 11:24:36 +0500

    Refactoring

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe '#component' do
     before { allow(helper).to receive(:render) }
 
-    it 'render component' do
+    it 'render a component' do
       expect(helper).to receive(:render).with('components/page/page', {})
       helper.component('page')
     end
 
-    context 'when path with namespace' do
-      it 'render component' do
+    context 'when the path with a namespace' do
+      it 'render a component' do
         expect(helper).to receive(:render).with('components/admin/admin_page', {})
         helper.component('admin_page')
       end
@@ -20,7 +20,7 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe '#с' do
     before { allow(helper).to receive(:render) }
 
-    it 'render component' do
+    it 'render a component' do
       expect(helper).to receive(:render).with('components/page/page', {})
       helper.c('page')
     end
