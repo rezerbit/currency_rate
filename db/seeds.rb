2459ad8d7270962d700a465b40c927dafffb8c96
rezerbit <rezerbit@gmail.com>
2018-12-20 08:37:24 +0500

    Chore: Add seeds [ci skip]

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 # frozen_string_literal: true
+
+FetchedRate.create(value: 12.1234)
+FetchedRate.create(value: 67.1125, overridden: true)
+ForcedRate.create(value: 60.37, expires_at: Time.current + 1.day)
